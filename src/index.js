28490e02d96059cd61aa58881f0f28f45b400aa9
brunoclaumari <brunoclaumari@gmail.com>
2020-07-29 22:15:59 -0300

    Criação cadastro de categorias

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './reset.css';
+import Home from './pages/Home';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+
+//Desafio master blaster na descrição
+const Pagina404 = () => (<div> Página 404 </div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter >
+        <Switch >
+            < Route path="/" component={Home} exact />
+            < Route path="/cadastro/video" component={CadastroVideo} />
+            < Route path="/cadastro/categoria" component={CadastroCategoria} />
+            < Route component={Pagina404} />
+        </Switch >
+    </BrowserRouter>,
+
+
+    document.getElementById('root')
 );
-
-
