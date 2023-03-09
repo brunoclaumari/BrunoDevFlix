56dff668b35f330a2bcc95b938ca8f27857410c7
brunoclaumari <brunoclaumari@gmail.com>
2023-03-09 00:12:16 -0300

    Alteração da url do servidor Node

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://brunodevflix.herokuapp.com';
+  : 'https://brunodevflix-backend-simples-2.vercel.app';
 
 export default {
   URL_BACKEND_TOP,
